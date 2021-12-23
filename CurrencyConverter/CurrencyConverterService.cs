38dbab47a63f24b73b639a33ab515ae33eb444bd
JJ <jonathansimonjones@gmail.com>
2021-12-23 13:38:10 +0000

    Summary: Returning all currenices via azure function using exchangeratesapi.io API. Added: Library project which contains models and services for managing project requirements. WIP.

---
+++ CurrencyConverter/CurrencyConverterService.cs
@@ -0,0 +1,19 @@
+﻿using CurrencyConverter.DataSources;
+using CurrencyConverter.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class CurrencyConverterService
+    {
+        public async Task<List<Currency>> GetAllCurrencies()
+        {
+            var exchangerateapi = new ExchangeRatesAPI();
+
+            return await exchangerateapi.GetAllCurrencies();
+
+        }
+    }
+}
--- currency-api-backend/currency-function.cs
+++ currency-api-backend/currency-function.cs
@@ -10,26 +10,11 @@ using Newtonsoft.Json;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text;
 
 namespace currency_api_backend
 {
-    public class Rates
-    {
-        public double USD { get; set; }
-        public double AUD { get; set; }
-        public double CAD { get; set; }
-        public double PLN { get; set; }
-        public double MXN { get; set; }
-    }
 
-    public class Root
-    {
-        public bool success { get; set; }
-        public int timestamp { get; set; }
-        public string @base { get; set; }
-        public string date { get; set; }
-        public Rates rates { get; set; }
-    }
 
 
     public static class Currency
@@ -42,44 +27,31 @@ namespace currency_api_backend
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var httpRequestMessage = new HttpRequestMessage(
-                HttpMethod.Get,
-                "http://api.exchangeratesapi.io/v1/latest?access_key=a86b6a6095ca487f7a481200fd1449ef")
-            {};
+            var currencyConverterService = new CurrencyConverter.CurrencyConverterService();
 
-            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            var result = await currencyConverterService.GetAllCurrencies();
 
-            if (httpResponseMessage.IsSuccessStatusCode)
+            var allCurrenciesAsText = new StringBuilder();
+
+            foreach(var currency in result)
             {
-                using var contentStream =
-                    await httpResponseMessage.Content.ReadAsStreamAsync();
-
-                try
-                {
-                    var result = await System.Text.Json.JsonSerializer.DeserializeAsync<Root>(contentStream);
-                    log.LogInformation("worked");
-                }
-                catch(Exception e)
-                {
-                    log.LogInformation(e.InnerException.ToString() + "\n" + e.Message);
-                }
-
-                log.LogInformation("finished.");
+                allCurrenciesAsText.AppendLine(currency.Name);
             }
 
-            string name = req.Query["name"];
+            //log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string name = req.Query["name"];
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
-            return new OkObjectResult(responseMessage);
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(allCurrenciesAsText.ToString());
         }
     }
 }
